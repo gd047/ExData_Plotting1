a6e620d456ac16dce0a591b6d4a6ef3198f900c8
gd047 <gd0047@gmail.com>
2014-05-29 10:11:16 +0300

    minor

--- best.R
+++ best.R
@@ -3,36 +3,36 @@ best <- function(state, outcome){
   #### load dplyr and helper functions ######################################
   require(dplyr)
   
-  require(devtools)
+  #  require(devtools)
   # https://gist.github.com/skranz/9681509 or 
-  source_gist("https://gist.github.com/jknowles/10888768")
+  #  source_gist("https://gist.github.com/jknowles/10888768")
   
-  #   #' Internal function used by s_filter, s_select etc.
-  #   eval.string.dplyr = function(.data, .fun.name, ...) {
-  #     args = list(...)
-  #     args = unlist(args)
-  #     code = paste0(.fun.name,"(.data,", paste0(args, collapse=","), ")")
-  #     df = eval(parse(text=code,srcfile=NULL))
-  #     df  
-  #   }
-  #   
-  #   #' Modified version of dplyr's select that uses string arguments
-  #   #' @export
-  #   s_select = function(.data, ...) {
-  #     eval.string.dplyr(.data,"select", ...)
-  #   }
-  #   
-  #   #' Modified version of dplyr's filter that uses string arguments
-  #   #' @export
-  #   s_filter = function(.data, ...) {
-  #     eval.string.dplyr(.data,"filter", ...)
-  #   }
-  #   
-  #   #' Modified version of dplyr's arrange that uses string arguments
-  #   #' @export
-  #   s_arrange = function(.data, ...) {
-  #     eval.string.dplyr(.data,"arrange", ...)
-  #   }
+  #' Internal function used by s_filter, s_select etc.
+  eval.string.dplyr = function(.data, .fun.name, ...) {
+    args = list(...)
+    args = unlist(args)
+    code = paste0(.fun.name,"(.data,", paste0(args, collapse=","), ")")
+    df = eval(parse(text=code,srcfile=NULL))
+    df  
+  }
+  
+  #' Modified version of dplyr's select that uses string arguments
+  #' @export
+  s_select = function(.data, ...) {
+    eval.string.dplyr(.data,"select", ...)
+  }
+  
+  #' Modified version of dplyr's filter that uses string arguments
+  #' @export
+  s_filter = function(.data, ...) {
+    eval.string.dplyr(.data,"filter", ...)
+  }
+  
+  #' Modified version of dplyr's arrange that uses string arguments
+  #' @export
+  s_arrange = function(.data, ...) {
+    eval.string.dplyr(.data,"arrange", ...)
+  }
   ##########################################################################
   
   data <- read.csv("outcome-of-care-measures.csv",
@@ -55,11 +55,11 @@ best <- function(state, outcome){
                   stop("invalid outcome"))
   )
   
-  res <-  data %.% 
+  res <-  data %>% 
     s_select(cols) %>%
     s_filter(paste0("!is.na(",last(cols),") & State == '",state,"'")) %>%  # exclude null
     s_arrange(paste0(last(cols),", Hospital.Name")) %>% # reorder 
     summarize(first(Hospital.Name))
   
-  return(res[[1]])
+  return(res[,1])
 }
